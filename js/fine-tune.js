0ffaa60d29e5e740fbe02506e58d0c98293da9fd
Ribin <ribinroy4@gmail.com>
2020-10-23 18:36:10 +0530

    Tasks done

---
+++ js/fine-tune.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    AOS.init();
+
+    $('.popup-video').click(function () {
+        var src = $(this).attr('video-src');
+        $('#video-pop-up .video-wrap').attr('src', src);
+        $('#video-pop-up').modal('show');
+    });
+
+    $('#video-pop-up').on('hidden.bs.modal', function () {
+        //clear src
+        $('#video-pop-up .video-wrap').attr('src', '');
+    });
+});
